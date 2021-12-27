d0d88587c610dd303ddf333ad39e2c9ba15c23e3
ZuoNicolas <nicolaszuo.contact@gmail.com>
2021-12-27 12:16:10 +0100

    add Switch

--- test.py
+++ test.py
@@ -7,29 +7,30 @@ import matplotlib.pyplot as plt
 
 env = Markov_chain('env0')
 
-nb_itérations = 10000
+nb_itérations = 20000
 reward = np.zeros((1,env.rewards.shape[1]))
 S = []
 env.reset()
 
 """
-Standar QL paramètre retenues:
-epsilon  : 0.05
+Standar QL paramètre du papier:
+epsilon  : 0.5
 alpha    : 0.7
 discount : 0.9
 """
 
-agent = Standar_Ql_agent(env, epsilon = 0.2, alpha = 0.7, 
+agent = Standar_Ql_agent(env, epsilon = 0.5, alpha = 0.2, 
                          discount=0.90, scal = [1, 1])
 
 for i in range(nb_itérations):
-    s = env.curent_state
+    s = env.current_state
     new_s, r = env.action(agent.act())
     agent.store(s, r, new_s)
     if agent.time_to_learn():
         agent.learn()
 
-    
+    if i % 2000 == 0:
+        agent.epsilon /= 2
     S += [s]
     reward = np.concatenate((reward,r), axis = 0)
 print(S)
@@ -42,3 +43,4 @@ plt.plot(cumsum[:,0], label="reward 1")
 plt.plot(cumsum[:,1], label="reward 2")
 plt.legend()
 plt.show()
+print()
